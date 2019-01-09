c0f241487f3f54862ca9c8fd44e243d66b797e88
aktunc-STMU <aktunc@gmail.com>
2019-01-09 07:10:15 -0600

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
   public static void main(String[] args) {
-    String s = "Hello world!";
+    String s = "Hello world!";  //Use 4 spaces as your indentation
     System.out.println(s);
    }
 }
